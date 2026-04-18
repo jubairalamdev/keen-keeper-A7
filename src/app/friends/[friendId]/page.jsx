2e74ca4f586a5eae03fbbb1d4606ec0b858e71fe
Jubair Alam <jubairalam.dev@gmail.com>
2026-04-18 23:28:45 +0600

    updated Friend ID and fixed Navbar Colors + Timeline Page Padding added.

--- src/app/friends/[friendId]/page.jsx
+++ src/app/friends/[friendId]/page.jsx
@@ -38,7 +38,7 @@ const FriendDetailsPage = () => {
 
     return (
         <div className="bg-base-200 py-20 text-base-content/90">
-            <div className="max-w-290 mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div className="max-w-290 px-5 md:px-8 xl:px-0 mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                 <div className="space-y-4">
                     <div className="bg-white rounded-md shadow-md shadow-base-content/5 transition p-5 text-center space-y-2">
                         <div className="flex justify-center items-center">
